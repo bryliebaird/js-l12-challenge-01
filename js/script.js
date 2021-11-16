9639c0eb6736217dae168a9c0004b3999b3fff76
brylie_baird <bryliebaird@gmail.com>
2021-11-15 20:48:32 -0600

    push

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getData = async function(){
+    const res = await fetch (
+        "https://picsum.photos/v2/list?limit=20"
+    );
+    const data = await res.json();
+    console.log(data);
+}
+
+button.addEventListener("click", function(){
+    getData();
+})
